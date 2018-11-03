55a29b8075bd6937829f22e921dcbb1bd89c8c0b
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-11-02 20:34:12 -0400

    Adds function to show all products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 var keys = require('./keys'),
     inquirer = require('inquirer'),
+    cTable = require('console.table'),
     mySQL = require('mysql'),
     connection = mySQL.createConnection({
         host: 'localhost',
@@ -10,6 +11,7 @@ var keys = require('./keys'),
         database: 'bamazon'
     });
 
+//Connect to DB
 connection.connect(function(err) {
     if (err) {
         //error received, end connection and throw
@@ -17,6 +19,42 @@ connection.connect(function(err) {
         return console.error('Error connecting to DB: ' + err);
     }
     
-    //connection successful
-    console.log('Connected to DB as id ' + connection.threadId);
+    //connection successful, begin application
+    showAllProducts();
 });
+
+function showAllProducts() {
+    var query = 'SELECT item_id, product_name, price FROM products ORDER BY item_id;';
+
+    var syntax = connection.query(query, function(err, resp) {
+        if (err) {
+            return console.log('Error from query ' + syntax.sql);
+        }
+
+        //Create array to hold formatted products
+        var products = [];
+
+        //Loop through rows in DB table
+        for(var i = 0; i < resp.length; i++) {
+            //Create new object
+            var product = {
+                ID: resp[i].item_id,
+                Name: resp[i].product_name,
+                'Price ($)': resp[i].price.toFixed(2) //formats decimal to 2 decimal places
+            };
+
+            //Pushes product object to new array
+            products.push(product);
+        }
+
+        //formats the consoled array to look like a table
+        console.table(products);
+
+        //get user input on which item they want to buy
+        itemToBuyInput();
+    });
+}
+
+function itemToBuyInput() {
+
+}
