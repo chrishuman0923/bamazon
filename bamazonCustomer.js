29105ffacd1ac3a6276735d7e5cba455d473305c
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-11-03 08:51:36 -0400

    Adds function to check available quantity in database before moving forward. Also added small function to get client-side variable of max db id.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,20 +16,40 @@ var keys = require('./keys'),
 connection.connect(function(err) {
     if (err) {
         //error received, end connection and throw
-        connection.end();
-        return console.error('Error connecting to DB: ' + err);
+        closeOnError('Error connecting to DB: ' + err);
     }
     
-    //connection successful, begin application
-    showAllProducts();
+    //connection successful
+    getMaxID();
 });
 
-function showAllProducts() {
-    var query = 'SELECT item_id, product_name, price FROM products ORDER BY item_id;';
+function getMaxID() {
+    //set query syntax
+    var query = 'SELECT MAX(item_id) AS max_id FROM products;';
 
+    //query db
     var syntax = connection.query(query, function(err, resp) {
         if (err) {
-            return console.log('Error from query ' + syntax.sql);
+            closeOnError('Error from query ' + syntax.sql);
+        }
+
+        //set client-side variable of the max id in the database
+        max_id = resp[0].max_id;
+
+        //begin application
+        showAllProducts();
+    });
+}
+
+function showAllProducts() {
+    //Set syntax of query
+    var query = 'SELECT item_id, product_name, price FROM products ORDER BY item_id;';
+
+    //query db
+    var syntax = connection.query(query, function(err, resp) {
+        if (err) {
+            //if error, log the query that was made
+            closeOnError('Error from query ' + syntax.sql);
         }
 
         //Create array to hold formatted products
@@ -46,9 +66,6 @@ function showAllProducts() {
 
             //Pushes product object to new array
             products.push(product);
-            
-            //Gets application side variable of maximum ID in DB
-            max_id = resp[i].item_id;
         }
 
         //formats the consoled array to look like a table
@@ -67,7 +84,7 @@ function itemToBuyInput() {
             message: 'Which item would you like to purchase (Enter ID)?',
             validate: function(input) {
                 //validation to determine if the id exists
-                if (!/^[1-9]+[0-9]*$$/gi.test(input) || input > max_id) {
+                if (!/^[1-9]+[0-9]*$/gi.test(input) || input > max_id) {
                     console.log('\nPlease enter a valid ID.');
                     return false;
                 } else {
@@ -89,6 +106,36 @@ function itemToBuyInput() {
             }
         }
     ]).then(function(answers) {
-        console.log(answers);
+        checkInventory(answers);
+    }).catch(function(err) {
+        closeOnError('Error received: ' + err);
     });
+}
+
+function checkInventory(input) {
+    //set query syntax
+    var query = 'SELECT quantity FROM products WHERE item_id=? LIMIT 1;';
+    
+    //query db
+    var syntax = connection.query(query, [parseInt(input.id)], function(err, resp) {
+        if (err) {
+            closeOnError('Error from query ' + syntax.sql);
+        }
+
+        //is there enough inventory for what was requested
+        if (input.quantity > resp[0].quantity) {
+            //no...restart application so user can decide again
+            console.log('\nInsufficient quantity in stock!\n');
+
+            //provides slight delay in execution so user can see response
+            setTimeout(getMaxID, 2000);
+        } else {
+            console.log('Make purchase!');
+        }
+    });
+}
+
+function closeOnError(errMsg) {
+    //connection.end();
+    return console.error(errMsg);
 }
